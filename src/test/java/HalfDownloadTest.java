48035613f843f33abbc671f222ddb3b35a6baa5d
Igor <ghostey1989@gmail.com>
2021-08-09 20:35:46 +0300

    custom ExpectedCondition was added

--- src/test/java/HalfDownloadTest.java
+++ src/test/java/HalfDownloadTest.java
@@ -1,22 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class HalfDownloadTest {
+    WebDriverWait wait;
     private WebDriver driver;
     private Integer percent;
     private String stringPercent;
@@ -31,6 +25,7 @@ public class HalfDownloadTest {
     public void beforeTest() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 20);
     }
 
     @AfterEach
@@ -45,12 +40,16 @@ public class HalfDownloadTest {
         driver.get("https://www.seleniumeasy.com/test/bootstrap-download-progress-demo.html");
         WebElement downloadButton = driver.findElement(By.id("cricle-btn"));
         downloadButton.click();
-        do {
-            WebElement loadingPercent = driver.findElement(By.xpath("//div[@class='percenttext']"));
-            stringPercent = loadingPercent.getText();
-            percent = Integer.valueOf(stringPercent.substring(0, stringPercent.length()-1));
+        wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                WebElement loadingPercent = driver.findElement(By.xpath("//div[@class='percenttext']"));
+                stringPercent = loadingPercent.getText();
+                percent = Integer.valueOf(stringPercent.substring(0, stringPercent.length() - 1));
+                if (percent >= 50) return true;
+                else return false;
+            }
         }
-        while (percent <=50);
+      );
         driver.navigate().refresh();
     }
 }
