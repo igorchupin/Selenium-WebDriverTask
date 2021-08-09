76ea1fd590496a135d0a872a55b8c697dc71c2f2
Igor <ghostey1989@gmail.com>
2021-08-09 22:35:17 +0300

    Stream API was used

--- src/test/java/MultiselectTest.java
+++ src/test/java/MultiselectTest.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MultiselectTest {
     private WebDriver driver;
@@ -41,11 +42,11 @@ public class MultiselectTest {
         selectObject.selectByValue("Texas");
         selectObject.selectByVisibleText("Washington");
         List<WebElement> selectedOptions = selectObject.getAllSelectedOptions();
-        for (int i = 0; i < selectedOptions.size(); i++) {
-            result = selectedOptions.get(i).getText().contains("New Jersey") || selectedOptions.get(i).getText().contains("Texas")
-                                                                        || selectedOptions.get(i).getText().contains("Washington");
-            if (result == false) {break;}
-        }
-       assertTrue(result, "Not all elements or incorrect elements were selected failed at point");
-        }
+        Stream<WebElement> expected = selectedOptions.stream();
+        result = expected.allMatch(x -> x.getText().contains("Texas") || x.getText().contains("Washington")
+                                                                      || x.getText().contains("New Jersey"));
+
+        assertTrue(result, "Not all elements or incorrect elements were selected failed at point");
     }
+}
+
